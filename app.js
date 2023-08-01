f2d365daded28ec1c2368990f4d2f241281c98a7
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-08-01 16:07:14 +0200

    Fix styling and placeholders for Data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const baseUrl =  "https://api.blockchain.com/v3/exchange/l2/"
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
