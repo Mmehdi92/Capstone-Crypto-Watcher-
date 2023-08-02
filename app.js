5aa1c132c10be5e02487b85f555ed4f2fcfe68db
Mehdi Zymberi <m.zymberi@student.avans.nl>
2023-08-02 09:57:54 +0200

    retrieve data from open api blockchain and display on the homepage

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 import bodyParser from "body-parser";
-import express from "express"
+import express from "express";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
-const baseUrl =  "https://api.blockchain.com/v3/exchange/l2/"
+const baseUrl = "https://api.blockchain.com/v3/exchange";
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs")
-})
+app.get("/", async (req, res) => {
+  try {
+    let result = await axios.get(baseUrl + "/tickers");
 
+    // filter the top 5 biggest volume 24h out and return them
+    let cryptoData = result.data;
+    cryptoData.sort((a, b) => b.volume_24h - a.volume_24h);
+    let top5BiggestVolume = cryptoData.slice(0, 5);
+    console.log(top5BiggestVolume);
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
-})
+    res.render("index.ejs", { crypto: top5BiggestVolume});
+  } catch (error) {
+    console.log(error.error);
+  }
+});
+
+app.post("/", async (req, res) => {
+  const userInput = req.body.inputsearch;
+  console.log(userInput);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
